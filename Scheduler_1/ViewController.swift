6a5efd8108112a5a3004b7808c9c86292e969f8b
Benjamin Baker <benjamin.r.baker@duke.edu>
2016-10-15 20:39:19 -0400

    Initial Commit

---
+++ Scheduler_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scheduler_1
+//
+//  Created by Ben Baker on 10/15/16.
+//  Copyright © 2016 Ben Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
